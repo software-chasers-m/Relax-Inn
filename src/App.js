38797dc3f0665b408485c040940e7defa464c03a
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-08-21 15:55:33 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactUS from './components/ContactUs';
+import AboutUs from './components/AboutUs';
+import { withAuth0 } from '@auth0/auth0-react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <AboutUs />
+            </Route>
+            <Route path="/contact-us">
+              <ContactUS />
+            </Route>
+          </Switch>
+        </Router>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withAuth0(App);

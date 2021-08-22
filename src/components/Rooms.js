510116fe8b2d65f80e37ba450d19a3a455468af1
tahany777 <tahanymohammed777@hotmail.com>
2021-08-22 13:57:07 +0300

    n

---
+++ src/components/Rooms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Rooms extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Rooms
